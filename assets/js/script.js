cb4b607a0468f2e1b0e166b75ce0c8ceebb43644
Mayara Silva <devmayara11@gmail.com>
2022-06-28 10:42:03 -0300

    Estruturando o validador

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,46 @@
 
+let validator = {
+    handleSubmit: (event)=> {
+        event.preventDefault();
 
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        for(let i=0; i<inputs.length; i++) {
+            let input = inputs[i];
+            let check = validator.checkInput(input);
+            if(check !== true) {
+                send = false;
+                // exibir o error
+            }
+        }
+
+        if(send) {
+            form.submit();
+        }
+    },
+    checkInput: (input)=> {
+        let rules = input.getAttribute('data-rules');
+        if(rules !== null) {
+            rules = rules.split('|');
+            for(let i in rules) {
+                let rDetail = rules[i].split('=');
+                switch(rDetail[0]) {
+                    case 'required':
+                        if(input.value == '') {
+                            return 'Campo obrigatório';
+                        }
+                    break;
+                    case 'min':
+                    
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+let form = document.querySelector('.validator');
+form.addEventListener('submit', validator.handleSubmit);

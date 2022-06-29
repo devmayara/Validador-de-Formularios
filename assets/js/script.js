33f9af9c4d9aa2c7e31ecc229ba7ad1f4459044d
Mayara Silva <devmayara11@gmail.com>
2022-06-29 08:37:23 -0300

    Verificações Personalizadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 
 let validator = {
-    handleSubmit: (event)=> {
+    handleSubmit:(event)=> {
         event.preventDefault();
-
         let send = true;
 
         let inputs = form.querySelectorAll('input');
@@ -22,44 +21,56 @@ let validator = {
             form.submit();
         }
     },
-    checkInput: (input)=> {
+    checkInput:(input) => {
         let rules = input.getAttribute('data-rules');
+
         if(rules !== null) {
             rules = rules.split('|');
-            for(let i in rules) {
-                let rDetail = rules[i].split('=');
-                switch(rDetail[0]) {
+            for(let k in rules) {
+                let rDetails = rules[k].split('=');
+                switch(rDetails[0]) {
                     case 'required':
                         if(input.value == '') {
                             return 'Campo obrigatório';
                         }
                     break;
                     case 'min':
-                    
+                        if(input.value.length < rDetails[2]) {
+                            return 'Mínimo de ' + rDetails[2] + ' caracteres';
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != '') {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'E-mail inválido';
+                            }
+                        }
                     break;
                 }
             }
         }
+
         return true;
     },
-    showError: (input, error)=> {
-        input.style.borderColor = 'red';
+    showError:(input, error) => {
+        input.style.borderColor = '#FF0000';
 
         let errorElement = document.createElement('div');
         errorElement.classList.add('error');
         errorElement.innerHTML = error;
 
-        input.parentElement.insertBefore(errorElement, input.ElementSide);
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
     },
-    clearErrors: ()=> {
+    clearErrors:() => {
         let inputs = form.querySelectorAll('input');
         for(let i=0; i<inputs.length; i++) {
             inputs[i].style = '';
-        }
+        } 
 
-        let errorElement = document.querySelectorAll('.error');
-        for(let i=0; i<errors.length; i++) {
-            errorElement[i].remove();
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0;i<errorElements.length;i++) {
+            errorElements[i].remove();
         }
     }
 };
